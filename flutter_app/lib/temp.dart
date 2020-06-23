3c2c987b8dc4865b98f4f4fcfa369b055df4128f
Ajinkya Dharashive <adharashive@gmail.com>
2020-06-23 23:59:09 +0530

    Dynamic ticket status cards added to main.dart. Ignore temp.dart!

--- flutter_app/lib/temp.dart
+++ flutter_app/lib/temp.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:tagservices/invoke.dart';
 
 //IMPORT PAGES
 import 'about.dart';
 import 'services.dart';
-import 'invoke.dart';
+//import 'menu.dart';
 import 'login.dart';
 
+void main(){
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -15,7 +18,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'TAG Services',
       theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
         primarySwatch: Colors.orange,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'TAG Services'),
@@ -25,6 +40,15 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
 
   final String title;
 
@@ -54,15 +78,21 @@ class _MyHomePageState extends State<MyHomePage> {
 
   Widget ticketCard(menu) {
     return Container(
-        width: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        width: MediaQuery
+            .of(context)
+            .size
+            .width,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
         child: Card(
           elevation: 5,
           shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(10)
           ),
           child: Container(
-            width: MediaQuery.of(context).size.width,
+            width: MediaQuery
+                .of(context)
+                .size
+                .width,
             padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
             child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -116,55 +146,75 @@ class _MyHomePageState extends State<MyHomePage> {
         )
     );
   }
+//		return Card(
+//      margin: EdgeInsets.all(30),
+//
+//      child: Padding(
+//        padding: const EdgeInsets.all(40.0),
+//        child : Container(
+//          child: Row(
+//            mainAxisAlignment: MainAxisAlignment.spaceAround,
+//            //crossAxisAlignment: CrossAxisAlignment.stretch,
+//            children : [
+//              Column(
+//                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//                children: <Widget>[
+//                  Text(
+//                    menu.icon,
+//                    style: TextStyle(
+//                      fontSize: 18.0,
+//                      color: Colors.lightBlue[600],
+//                    ),
+//                  ),
+//                  SizedBox(height: 5.0),
+//                  Text(
+//                    menu.text,
+//                    style: TextStyle(
+//                      fontSize: 12.0,
+//                      color: Colors.lightBlue[200],
+//                    ),
+//                  ),
+//                ],
+//              ),
+//            ],
+//          ),
+//        )
+//      )
+//    );
 
-// This works as well!!
-  Widget _buildTicketStatus(String id,String date,String status){
-    return Container(
-        height: 100,
-        padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(10),
-            boxShadow: [BoxShadow(
-                color: Color.fromRGBO(120, 95, 27, .3),
-                blurRadius: 20.0,
-                offset: Offset(0, 5)
-            )]
-        ),
-        child: Column(
-          children: <Widget>[
-            new Row(
-              children: <Widget>[
-                Text("Ticket ID : ",style: TextStyle(color: Colors.black, fontSize: 16,fontWeight: FontWeight.bold)),
-                SizedBox(width: 20,),
-                Text(
-                  "$id"
-                ),
-              ],
-            ),
-            SizedBox(height: 10,),
-            new Row(
-              children: <Widget>[
-                Text("Invoked Date : ",style: TextStyle(color: Colors.black, fontSize: 16,fontWeight: FontWeight.bold)),
-                SizedBox(width: 20,),
-                Text("$date"),
-              ],
-            ),
-            SizedBox(height: 10,),
-            new Row(
-              children: <Widget>[
-                Text("Status : ",style: TextStyle(color: Colors.black, fontSize: 16,fontWeight: FontWeight.bold)),
-                SizedBox(width: 20,),
-                Text("$status"),
-              ],
-            ),
-          ],
-        ),
-    );
-  }
+//		children: <Widget>[
+//			Expanded(
+//					child: SizedBox(
+//						height: 210,
+//						child: ListTile(
+//							title: Text('AC Number 2'),
+//							subtitle: Text('Ticket Number: 123456'),
+//							leading: Icon(
+//									Icons.ac_unit,
+//									color: Colors.blue[500]
+//							),
+//						),
+//					)
+//			)
+//		],
+//
+//		return ListTile(
+//			title: Text('AC Number 2'),
+//			subtitle: Text('Ticket Number: 123456'),
+//			leading: Icon(
+//			Icons.ac_unit,
+//			color: Colors.blue[500]
+//			),
+//		);
 
   @override
   Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: new AppBar(
         title: new Text("TAG Services"),
@@ -173,8 +223,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: ListView(
           children: <Widget>[
             new UserAccountsDrawerHeader(
-              accountName: new Text("Ajju"),
-              accountEmail: new Text("adharashive@gmail.com")
+                accountName: new Text("Ajju"),
+                accountEmail: new Text("adharashive@gmail.com")
             ),
             new ListTile(
               title: new Text("Home"),
@@ -204,7 +254,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
             ),
             new ListTile(
-              title: new Text("Invoke Ticket"),
+              title: new Text("New Ticket"),
               onTap: () {
                 Navigator.of(context).pop();
                 Navigator.push(context, new MaterialPageRoute(
@@ -215,7 +265,7 @@ class _MyHomePageState extends State<MyHomePage> {
             new ListTile(
               title: new Text("Logout"),
               onTap: () {
-                _save('false');
+                //prefs.setInt('login_flag',0);
                 Navigator.of(context).pop();
                 Navigator.push(context, new MaterialPageRoute(
                     builder: (BuildContext context) => new LoginPage() )
@@ -225,33 +275,10 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      body: ListView(
+			body: ListView(
         // ignore: non_constant_identifier_names
         children: menu.map((Menu) => ticketCard(Menu)).toList(),
-      )
-
-//This works as well..
-      /*Container(
-        child : Padding(
-          padding: const EdgeInsets.all(40.0),
-            child: Container(
-              child: Column(
-                children: <Widget>[
-                  _buildTicketStatus("1","18/06/2020","Pending"),
-                  SizedBox(height: 20,),
-                  _buildTicketStatus("2", "19/06/2020", "Approved")
-                ],
-              )
-            )
-        ),
-      )*/
+    )
     );
-  }
-
-  _save(String loginToken) async{
-    final prefs = await SharedPreferences.getInstance();
-    final key = 'loginToken';
-    final value = loginToken;
-    prefs.setString(key,value);
   }
 }
